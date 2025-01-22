f63f7195bfaffc767d288c06f06cd01fc43687ea
Tennyson McCalla <tennyson.mccalla@gmail.com>
2025-01-22 00:32:02 -0500

    chore: Update TypeScript and Jest configurations

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
+/** @type {import('jest').Config} */
 module.exports = {
-  testEnvironment: "node",
-  // Add Supabase environment setup
-  setupFiles: ["<rootDir>/tests/setup.js"],
-  // Optional: if we need to transform any files
-  transform: {},
-  // Optional: patterns to ignore
-  testPathIgnorePatterns: ["/node_modules/"]
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['**/tests/**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.test.json'
+    }]
+  }
 };
